d2eff37b837b019b08a36d55f38158a94a33f9f7
dobrinaborisova <dobrinaborisova@yahoo.com>
2017-02-15 21:38:45 +0200

    First commit for Homework - coding exercises 101

---
+++ Coding 01 Exam/Training Lab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  start:
+            double length = double.Parse(Console.ReadLine())*100;
+            double width = double.Parse(Console.ReadLine()) * 100;
+
+            double numberOfSpacesPerRow = Math.Truncate((width - 100)/70);
+            double numberOfSpacesPerColoumn = Math.Truncate((length / 120));
+            double numberOfWorkSpaces = (numberOfSpacesPerRow * numberOfSpacesPerColoumn) - 3;
+
+            Console.WriteLine(Math.Floor(numberOfWorkSpaces));
+
+         //   goto start;
+        }
+    }
+}
