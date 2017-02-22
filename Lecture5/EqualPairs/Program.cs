5324a54ee3ea44742f065f4ea0a7286f5f33dd1c
dobrinaborisova <dobrinaborisova@yahoo.com>
2017-02-22 16:40:32 +0200

    EqualPairs - Done!

--- Lecture5/EqualPairs/Program.cs
+++ Lecture5/EqualPairs/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EqualPairs
 {
@@ -10,104 +8,34 @@ namespace EqualPairs
     {
         static void Main(string[] args)
         {
-            double number = double.Parse(Console.ReadLine());
-            double EvenSum = 0.0;
-            double OddSum = 0.0;
-            double EvenMax = double.MinValue;
-            double EvenMin = double.MaxValue;
-            double OddMax = double.MinValue;
-            double OddMin = double.MaxValue;
+            int numberOfNumbers =  int.Parse(Console.ReadLine());
+            int firstSum = 0;
 
-            for (int i = 0; i < number; i++)
+            int sumDiff = 0;
+            List<int> numbers = new List<int>();
+            List<int> sum = new List<int>();
+
+            for (int i = 0; i < numberOfNumbers; i++)
             {
-                if (i % 2 != 0)
-                {
-                    double tempNumber = double.Parse(Console.ReadLine());
-                    if (tempNumber >= EvenMax)
-                    {
-                        EvenMax = tempNumber;
-                    }
-                    else
-                    {
-                        EvenMin = tempNumber;
-                    }
-                    EvenSum += tempNumber;
-                }
-                else
-                {
-                    double tempNumber = double.Parse(Console.ReadLine());
-                    if (tempNumber >= OddMax)
-                    {
-                        OddMax = tempNumber;
-                    }
-                    else
-                    {
-                        OddMin = tempNumber;
-                    }
-                    OddSum += tempNumber;
-                }
+                
+                sum.Add(int.Parse(Console.ReadLine()) + int.Parse(Console.ReadLine()));
             }
 
-            if (OddMin == double.MaxValue)
+            firstSum = sum.ElementAt(0);
+
+            for (int i = 1; i < sum.Count; i++)
             {
-                OddMin = OddMax;
-            }
-            if (OddMax == double.MinValue)
-            {
-                OddMax = OddMin;
-            }
-            if (EvenMin == double.MaxValue)
-            {
-                EvenMin = EvenMax;
-            }
-            if (EvenMax == double.MinValue)
-            {
-                EvenMax = EvenMin;
+                sumDiff = Math.Max(Math.Abs(sum.ElementAt(i) - sum.ElementAt(i - 1)), sumDiff);
             }
 
-            StringBuilder builder = new StringBuilder();
-
-            builder.AppendLine(string.Format("OddSum={0},", OddSum));
-
-            if (OddMin != double.MaxValue)
+            if (sumDiff == 0)
             {
-                builder.AppendLine(string.Format("OddMin={0},", OddMin));
+                Console.WriteLine("Yes, value = {0}", firstSum);
             }
             else
             {
-                builder.AppendLine(string.Format("OddMin={0},", "No"));
+                Console.WriteLine("No, maxdiff = {0}", sumDiff);
             }
-
-            if (OddMax != double.MinValue)
-            {
-                builder.AppendLine(string.Format("OddMax={0},", OddMax));
-            }
-            else
-            {
-                builder.AppendLine(string.Format("OddMax={0},", "No"));
-            }
-
-            builder.AppendLine(string.Format("EvenSum={0},", EvenSum));
-
-            if (EvenMin != double.MaxValue)
-            {
-                builder.AppendLine(string.Format("EvenMin={0},", EvenMin));
-            }
-            else
-            {
-                builder.AppendLine(string.Format("EvenMin={0},", "No"));
-            }
-
-            if (EvenMax != double.MinValue)
-            {
-                builder.AppendLine(string.Format("EvenMax={0}", EvenMax));
-            }
-            else
-            {
-                builder.AppendLine(string.Format("EvenMax={0}", "No"));
-            }
-
-            Console.WriteLine(builder.ToString());
         }
     }
 }
--- Lecture5/OddEven Position/Program.cs
+++ Lecture5/OddEven Position/Program.cs
@@ -4,12 +4,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OddEven_Position
+namespace OddEvenPositions
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double number = double.Parse(Console.ReadLine());
+            double EvenSum = 0.0;
+            double OddSum = 0.0;
+            double EvenMax = double.MinValue;
+            double EvenMin = double.MaxValue;
+            double OddMax = double.MinValue;
+            double OddMin = double.MaxValue;
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    double tempNumber = double.Parse(Console.ReadLine());
+                    if (tempNumber >= EvenMax)
+                    {
+                        EvenMax = tempNumber;
+                    }
+
+                    if (tempNumber <= EvenMin)
+                    {
+                        EvenMin = tempNumber;
+                    }
+                    EvenSum += tempNumber;
+                }
+                else
+                {
+                    double tempNumber = double.Parse(Console.ReadLine());
+                    if (tempNumber >= OddMax)
+                    {
+                        OddMax = tempNumber;
+                    }
+
+                    if (tempNumber <= OddMin)
+                    {
+                        OddMin = tempNumber;
+                    }
+                    OddSum += tempNumber;
+                }
+            }
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine(string.Format("OddSum={0},", OddSum));
+
+            if (OddMin != double.MaxValue)
+            {
+                builder.AppendLine(string.Format("OddMin={0},", OddMin));
+            }
+            else
+            {
+                builder.AppendLine(string.Format("OddMin={0},", "No"));
+            }
+
+            if (OddMax != double.MinValue)
+            {
+                builder.AppendLine(string.Format("OddMax={0},", OddMax));
+            }
+            else
+            {
+                builder.AppendLine(string.Format("OddMax={0},", "No"));
+            }
+
+            builder.AppendLine(string.Format("EvenSum={0},", EvenSum));
+
+            if (EvenMin != double.MaxValue)
+            {
+                builder.AppendLine(string.Format("EvenMin={0},", EvenMin));
+            }
+            else
+            {
+                builder.AppendLine(string.Format("EvenMin={0},", "No"));
+            }
+
+            if (EvenMax != double.MinValue)
+            {
+                builder.AppendLine(string.Format("EvenMax={0}", EvenMax));
+            }
+            else
+            {
+                builder.AppendLine(string.Format("EvenMax={0}", "No"));
+            }
+
+            Console.WriteLine(builder.ToString());
         }
     }
 }
