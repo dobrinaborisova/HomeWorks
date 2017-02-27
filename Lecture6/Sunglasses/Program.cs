522e85f3007a937ca9f05660232d83f2849fe329
Dobrina Borisova <Dobrina Borisova@INATHINKPAD>
2017-02-27 22:23:07 +0200

    one last thing to fix with the sunglasses

--- Lecture6/Sunglasses/Program.cs
+++ Lecture6/Sunglasses/Program.cs
@@ -11,6 +11,28 @@ namespace Sunglasses
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            int size = 5 * n;
+
+            string firstLast = string.Concat(Enumerable.Repeat("*", 2 * n).Concat(Enumerable.Repeat(" ", n).Concat(Enumerable.Repeat("*", 2 * n))));
+            Console.WriteLine(firstLast);
+            //Console.WriteLine();
+            for (int i = 0; i < n - 2; i++)
+            {
+                string middle;
+                if (i == ((n - 2) / 2))
+                {
+                    middle = string.Concat(Enumerable.Repeat("*", 1).Concat(Enumerable.Repeat("/", 2 * n - 2).Concat(Enumerable.Repeat("*", 1).Concat(Enumerable.Repeat(" ", n).Concat(Enumerable.Repeat("*", 1).Concat(Enumerable.Repeat("/", 2 * n - 2).Concat(Enumerable.Repeat("*", 1))))))));
+                }
+
+                else
+                {
+                    middle = string.Concat(Enumerable.Repeat("*", 1).Concat(Enumerable.Repeat("/", 2 * n - 2).Concat(Enumerable.Repeat("*", 1).Concat(Enumerable.Repeat("|", n).Concat(Enumerable.Repeat("*", 1).Concat(Enumerable.Repeat("/", 2 * n - 2).Concat(Enumerable.Repeat("*", 1))))))));
+
+                }
+                Console.WriteLine(middle);
+            }
+                //Console.WriteLine();
+            Console.WriteLine(firstLast);
         }
     }
 }
