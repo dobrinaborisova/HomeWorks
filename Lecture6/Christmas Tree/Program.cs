7e284e7d94fab7a43e7a8713ce31fa22e9b40e7f
dobrinaborisova <dobrinaborisova@yahoo.com>
2017-02-27 17:19:00 +0200

    Diamonds - started

---
+++ Lecture6/Christmas Tree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Christmas_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Concat(Enumerable.Repeat(" ", n+1).Concat(Enumerable.Repeat("|", 1))));
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(new string(' ', n - i));
+                Console.WriteLine(string.Concat(Enumerable.Repeat("*", i).Concat(Enumerable.Repeat(" ", 1)).Concat(Enumerable.Repeat("|", 1).Concat(Enumerable.Repeat(" ", 1).Concat(Enumerable.Repeat("*", i))))));
+            }
+        }
+    }
+}
---
+++ Lecture6/Diamand/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 != 0)
+            {
+
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write(new string('_', n - i));
+                    Console.Write(string.Concat(Enumerable.Repeat("*", i)));
+                    Console.WriteLine(string.Concat(Enumerable.Repeat("_", n - i)));
+
+                }
+                for (int i = n - 1; i > 0; i--)
+                {
+                    Console.Write(new string('_', n - i));
+                    Console.Write(string.Concat(Enumerable.Repeat("*_", i)));
+                    Console.WriteLine(string.Concat(Enumerable.Repeat("_", n - i)));
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write(new string('-', n - i));
+                    Console.Write(string.Concat(Enumerable.Repeat("*_", i)));
+                    Console.WriteLine(string.Concat(Enumerable.Repeat("-", n - i)));
+
+                }
+                for (int i = n - 1; i > 0; i--)
+                {
+                    Console.Write(new string('-', n - i));
+                    Console.Write(string.Concat(Enumerable.Repeat("*-", i)));
+                    Console.WriteLine(string.Concat(Enumerable.Repeat("-", n - i)));
+                }
+            }
+        }
+    }
+}
