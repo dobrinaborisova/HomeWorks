187d173e5c84c82044f43bbeebed3bfa655882da
Dobrina Borisova <Dobrina Borisova@INATHINKPAD>
2017-03-07 00:04:01 +0200

    Lecture7- first 5 tasks - done.

---
+++ Lecture7/03. Powers of Two/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Powers_of_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+
+            for (double i = 0; i <= n; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
---
+++ Lecture7/06. Number in Range [1...100]/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number1to100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Еnter a number in the range [1...100]: ");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n > 100 || n < 1)
+            {
+                Console.WriteLine("Invalid number!");
+                Console.WriteLine("Еnter a number in the range [1...100]: ");
+                n = int.Parse(Console.ReadLine());
+            }
+            if (n <= 100 && n >= 1)
+            {
+                Console.WriteLine("The number is: {0}", n);
+            }
+        }
+    }
+}
