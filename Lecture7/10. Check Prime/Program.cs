ef7858f9326aa69eddec07f6ee8f6b8028bf56ea
dobrinaborisova <dobrinaborisova@yahoo.com>
2017-03-07 17:20:02 +0200

    some more tasks - done

---
+++ Lecture7/10. Check Prime/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Check_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 2)
+            {
+                Console.WriteLine("Not Prime");
+            }
+
+            else 
+            {
+                for (int i = 0; i < n; i++)
+                {
+
+                }
+
+                Console.WriteLine("Prime");
+            }
+        }
+    }
+}
---
+++ Lecture7/11. Enter Even Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Enter_Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Еnter even number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            //invalid number
+            while (n % 2 != 0)
+            {
+                Console.WriteLine("The number is not even.");
+                Console.Write("Еnter even number: ");
+                try
+                {
+                    n = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid number!");
+                }
+            }
+            //valid number
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Even number entered: {0}", n);
+            }
+        }
+    }
+}
